a34e2a016f3ff486f7e555a71221552211774b5f
Ubuntu <ubuntu@ip-172-31-2-234.ap-south-1.compute.internal>
2025-07-21 09:44:13 +0000

    minikube

---
+++ minikube/minikube.sh
@@ -0,0 +1,33 @@
+
+# Installing Docker 
+
+#!/bin/bash
+sudo apt update
+sudo apt install docker.io -y
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
+sudo systemctl restart docker
+sudo chmod 777 /var/run/docker.sock
+
+# Installing Kubectl
+#!/bin/bash
+sudo apt update
+sudo apt install curl -y
+sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
+sudo chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version --client
+
+# Inatalling Minikube using binary 
+
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+# create minikube-cluster
+minikube start 
+
+# testing ...
+kubeclt get nodes
+
+# Deleting Minikube 
+minikube delete --all
